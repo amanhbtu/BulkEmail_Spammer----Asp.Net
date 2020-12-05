3dbe882e56fd75d29885b0069da725b658cc9dbe
AMAN MAURYA <amanmaurya1122@outlook.com>
2020-12-05 12:57:28 +0530

    chk 213

---
+++ BulkEmail_Spammer/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BulkEmail_Spammer.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input id="txtemailid" runat="server" placeholder="Email ID" type="email"/><br/>
+            <input id="txtnofemails" runat="server" placeholder="Number of Emails" type="number"/><br/>
+            <input id="txtsub" runat="server" placeholder="Subject"/><br/>
+            <input id="txtmsg" runat="server" placeholder="Message" type="text"/><br/>
+            <asp:Button ID="btnAction" runat="server" Text="Go" OnClick="btnAction_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ BulkEmail_Spammer/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BulkEmail_Spammer
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAction_Click(object sender, EventArgs e)
+        {
+            if (txtemailid.Value.Trim() != "")
+            {
+                int n = Convert.ToInt32(txtnofemails.Value);
+                int a = 0;
+                while (++a<=n)
+                {
+                    sendmail(txtemailid.Value, otp() + " " + txtmsg.Value + " " + otp(), txtsub.Value + " " + otp());
+                }
+            }
+        }
+
+        public void sendmail(String to, String msg, String sub)
+        {
+            MailMessage mm = new MailMessage("noreply@gmail.com", to);
+            mm.Subject = sub;
+            mm.Body = msg;
+            mm.IsBodyHtml = true;
+            mm.ReplyTo = mm.Sender = new MailAddress("noreply@gmail.com");
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com,587");
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            NetworkCredential NetworkCred = new NetworkCredential("tansharma8787@gmail.com", "Qwerty@123");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = NetworkCred;
+            smtp.Port = 587;
+            smtp.Send(mm);
+            //for alert
+            //ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Email sent.');", true);
+        }
+        public String otp()
+        {
+            string alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            //string small_alphabets = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "1234567890";
+            string characters = numbers + alphabets;
+            int length = 6;
+            string otp = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                string character = string.Empty;
+                do
+                {
+                    int index = new Random().Next(0, characters.Length);
+                    character = characters.ToCharArray()[index].ToString();
+                } while (otp.IndexOf(character) != -1);
+                otp += character;
+            }
+            return otp;
+        }
+    }
+}
